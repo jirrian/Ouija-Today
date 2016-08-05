29b432ddfd00b456a31eb06c58954dbf635bd70c
Jillian Zhong <jillianzhong@yahoo.com>
2016-08-05 02:05:46 -0400

    implemented finder movement and tested'

---
+++ Assets/Scripts/finderMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finderMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Input.acceleration.x * 0.3f, Input.acceleration.y * 0.3f, 0);
+	}
+}
